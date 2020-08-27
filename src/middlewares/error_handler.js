3b3ff0cf6182c8ff0995819b9e31b8fbd6bb3c1d
Sandro <59439519+sandro-simplon@users.noreply.github.com>
2020-08-27 14:16:01 +0200

    Update error_handler.js

--- src/middlewares/error_handler.js
+++ src/middlewares/error_handler.js
@@ -1,13 +1,14 @@
 module.exports = (error, request, response, next) => {
-  const { title, description } = error;
+  const { title } = error;
+  let { description } = error;
 
   const status = error.status || 500;
-  if (status === 500) {
-    description = "Serveur cassé. Revenez plus tard.";
+  if (status == 500) {
+    description = 'Serveur cassé. Revenez plus tard.';
   }
 
-  response.status(error.status).json({
+  response.status(status).json({
     title,
-    description
+    description,
   });
 };
