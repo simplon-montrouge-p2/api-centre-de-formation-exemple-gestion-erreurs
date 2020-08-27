4a009f33a0fe869364f1ef7c5b09618a36b50d61
Sandro Micalizzi <65682590+sandrocode@users.noreply.github.com>
2020-08-27 09:59:09 +0200

    Erreur 500

--- src/middlewares/error_handler.js
+++ src/middlewares/error_handler.js
@@ -1,5 +1,11 @@
 module.exports = (error, request, response, next) => {
   const { title, description } = error;
+
+  const status = error.status || 500;
+  if (status === 500) {
+    description = "Serveur cassé. Revenez plus tard.";
+  }
+
   response.status(error.status).json({
     title,
     description
